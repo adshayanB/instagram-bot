61c4a7e334defc8292f528302ed7274b0e7a1cfc
adshayanB <41243143+adshayanB@users.noreply.github.com>
2020-01-27 10:01:47 -0500

    Automated follow and unfollow processes

--- bot.py
+++ bot.py
@@ -6,40 +6,50 @@ class InstaBot:
     def __init__(self,username,password):
         self.driver = webdriver.Chrome('/Users/abalendra/Downloads/chromedriver')
         self.driver.get('https://instagram.com')
-        sleep(2)
+        sleep(4)
         self.driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[2]/p/a").click()
-        sleep(2)
+        sleep(4)
         self.driver.find_element_by_xpath("//input[@name=\"username\"]").send_keys(username)
         self.driver.find_element_by_xpath("//input[@name=\"password\"]").send_keys(password)
         self.driver.find_element_by_xpath("//button[@type='submit']").click()
         print("Logged in")
-        sleep(2)
+        sleep(4)
     def follow (self, account_name):
         self.driver.get(f'https://instagram.com/{account_name}')
-        sleep(2)
+        sleep(4)
         follow_button = self.driver.find_element_by_css_selector('button')
         if(follow_button.text !='Following'):
             follow_button.click()
-            print(f'You are know following {account_name}')
-            sleep(2)
+            print(f'You are now following {account_name}')
+            sleep(4)
         else:
-            print(f"Your already following {account_name}")
+            print(f"You are already following {account_name}")
+    def unfollow (self, account_name):
+        self.driver.get(f'https://instagram.com/{account_name}')
+        sleep(2)
+        follow_button = self.driver.find_element_by_css_selector('button')
+        if(follow_button.text =='Following'):
+            follow_button.click()
+            confirmButton = self.driver.find_element_by_xpath('//button[text() = "Unfollow"]')
+            confirmButton.click()
+            print(f'Sucessfully unfollowed {account_name}')
+        else:
+            print(f"You don't follow {account_name}")
+        sleep(2)
+
+    def close (self):
+        self.driver.quit()
 
 
 
 
+user = input("Enter your instagram account: ")
+password = input("Enter your instagram password: ")
+account_follow = input("Enter the account name you would like to follow: ")
+account_unfollow = input("Enter the account you would like to unfollow: ")
 
+acc = InstaBot(user,password)
+acc.follow(account_follow)
+acc.unfollow(account_unfollow)
+acc.close()
 
-
-
-
-
-
-
-
-
-
-
-
-acc = InstaBot('instagram_username','instagram_password')
-acc.follow('username_of_account_you_want_to_follow')
